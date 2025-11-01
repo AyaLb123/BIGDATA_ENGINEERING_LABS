5e380550565fad396c21d80656c759554027e601
AyaLb123 <lbsireaya@gmail.com>
2025-11-01 02:01:35 +0100

    Ajout de tp kafka

--- hadoop_lab/src/main/java/edu/ensias/hadoop/hdfslab/HadoopFileStatus.java
+++ hadoop_lab/src/main/java/edu/ensias/hadoop/hdfslab/HadoopFileStatus.java
@@ -6,34 +6,22 @@ import org.apache.hadoop.fs.*;
 
 public class HadoopFileStatus {
     public static void main(String[] args) {
-        // Vérifier que les 3 arguments sont fournis
-        if (args.length != 3) {
-            System.out.println("Usage: HadoopFileStatus <chemin_dossier> <nom_fichier> <nouveau_nom_fichier>");
-            System.exit(1);
-        }
-
-        String cheminDossier = args[0];  // ex: /user/root/input
-        String nomFichier = args[1];     // ex: achats.txt
-        String nouveauNom = args[2];     // ex: nouveau_nom.txt
-
         Configuration conf = new Configuration();
-
-        // Charger explicitement la configuration Hadoop
-        conf.addResource(new Path("/usr/local/hadoop/etc/hadoop/core-site.xml"));
-        conf.addResource(new Path("/usr/local/hadoop/etc/hadoop/hdfs-site.xml"));
+        FileSystem fs;
 
         try {
-            FileSystem fs = FileSystem.get(conf);
-            Path filepath = new Path(cheminDossier, nomFichier);
+            fs = FileSystem.get(conf);
+            Path filepath = new Path("/user/root/input/purchases.txt");
 
-            // Vérifier si le fichier existe
+            // Vérifier si le fichier existe avant de récupérer le FileStatus
             if (!fs.exists(filepath)) {
-                System.out.println("File does not exist: " + filepath);
+                System.out.println("File does not exist");
                 System.exit(1);
             }
 
-            // Lire les métadonnées du fichier
             FileStatus infos = fs.getFileStatus(filepath);
+
+            System.out.println(Long.toString(infos.getLen()) + " bytes");
             System.out.println("File Name: " + filepath.getName());
             System.out.println("File Size: " + infos.getLen());
             System.out.println("File owner: " + infos.getOwner());
@@ -54,15 +42,12 @@ public class HadoopFileStatus {
             }
 
             // Renommer le fichier
-            Path newFilePath = new Path(cheminDossier, nouveauNom);
-            if (fs.rename(filepath, newFilePath)) {
-                System.out.println("File renamed successfully to: " + nouveauNom);
-            } else {
-                System.out.println("Failed to rename file.");
-            }
+            fs.rename(filepath, new Path("/user/root/input/achats.txt"));
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
+
